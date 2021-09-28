83c19f910809104f2d429e487eb0bc45ee58fcda
Shining Wing <shiningwing@protonmail.com>
2021-09-27 23:20:56 -0300

    Undo version change

--- buildSrc/src/main/kotlin/AppConfigs.kt
+++ buildSrc/src/main/kotlin/AppConfigs.kt
@@ -2,6 +2,6 @@ object AppConfigs {
     const val COMPILE_SDK_VERSION = 31
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 31
-    const val VERSION_CODE = 17
-    const val VERSION_NAME = "2.6"
+    const val VERSION_CODE = 16
+    const val VERSION_NAME = "2.5"
 }
